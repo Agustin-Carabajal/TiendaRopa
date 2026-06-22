2840d8a20447fbbe3035a5349629165ec4946ac5
Agustin-Carabajal <carabajal.agustin.ezequiel@gmail.com>
2026-06-22 10:48:29 -0300

    Repositorio de Productos

---
+++ TiendaRopa.Repositorio/Repositorios/Producto/IMarcaRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using TiendaRopa.Shared.DTO.Producto_y_mas;
+
+namespace TiendaRopa.Repositorio.Repositorios.Producto
+{
+    public interface IMarcaRepositorio
+    {
+        Task<List<MarcaMostrarDTO>> SelectListaMarcas();
+    }
+}
---
+++ TiendaRopa.Repositorio/Repositorios/Producto/TalleRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using TiendaRopa.BD.Datos;
+using TiendaRopa.BD.Datos.Entity;
+using TiendaRopa.Repositorio.Repositorios.Generico;
+using TiendaRopa.Shared.DTO.Producto_y_mas;
+using Microsoft.EntityFrameworkCore;
+
+namespace TiendaRopa.Repositorio.Repositorios.Producto
+{
+    public class TalleRepositorio : Repositorio<Talle>, IRepositorio<Talle>, ITalleRepositorio
+    {
+        private readonly AppDbContext context;
+
+        public TalleRepositorio(AppDbContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<TalleMostrarDTO>> SelectListaTalles()
+        {
+            var lista = await context.Talles.Select(x => new TalleMostrarDTO
+            {
+                IdTalle = x.Id,
+                NombreTalle = x.NombreTalle
+            }).ToListAsync();
+            return lista;
+        }
+    }
+}
